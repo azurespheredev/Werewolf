cd09b1f188b58877aba4dc00cd890864861b8a51
azurespheredev <24680042+azurespheredev@users.noreply.github.com>
2025-06-23 17:39:03 -0400

    fix: tailwindcss configuration issues

--- src/renderer/pages/WelcomePage.tsx
+++ src/renderer/pages/WelcomePage.tsx
@@ -1,9 +1,7 @@
-import '../App.css';
-
 export default function WelcomePage() {
   return (
     <div>
-      <h1 className="text-4xl">Hello</h1>
+      <h1 className="text-xl">Hello</h1>
     </div>
   );
 }
