309d7d50569f1ea65cc3b6ce3c1aded6f5d4060e
azurespheredev <24680042+azurespheredev@users.noreply.github.com>
2025-06-24 16:35:33 -0400

    feat: created an shaking animation to the road sign
    Co-authored-by: AzureSphere <dustin.jason.lee@gmail.com>
    Co-authored-by: Morgan Mastrangelo <mmastrangelo1120@gmail.com>

--- src/renderer/pages/WelcomePage.tsx
+++ src/renderer/pages/WelcomePage.tsx
@@ -5,16 +5,16 @@ import Input from '../components/shared/Input';
 export default function WelcomePage() {
   return (
     <BackgroundBox
-      src="/images/bg_sunset.jpg"
+      src="/images/bg_welcome.jpg"
       className="flex justify-center items-center w-full h-screen"
     >
-      <img
+      {/* <img
         alt="logo"
         src="/images/logo.png"
         className="absolute top-4 left-4 w-1/4 h-auto"
-      />
+      /> */}
 
-      <div className="absolute right-0 bottom-0 w-96">
+      <div className="absolute right-0 bottom-0 w-96 anim-shake-to-left">
         <img alt="road_sign" src="/images/road_sign.png" />
 
         <form className="flex flex-col items-center gap-0 absolute top-20 right-1/2 w-72 translate-x-1/2 rotate-3 p-1 text-red-950">
