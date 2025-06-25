44173667cf2a3684d24f150430070c0558537c65
azurespheredev <24680042+azurespheredev@users.noreply.github.com>
2025-06-25 03:41:30 -0400

    style: organize the server codebase

--- src/api/utils/constants.ts
+++
@@ -1,8 +0,0 @@
-export const GOOGLE_CALENDAR_API_URL = "https://www.googleapis.com/calendar/v3";
-export const GOOGLE_OAUTH_BASE_URL = "https://oauth2.googleapis.com";
-export const EXCLUDED_CALENDAR_IDS = [
-  "en.usa#holiday@group.v.calendar.google.com",
-];
-
-export const CALENDAR_EVENT_BATCH_SIZE = 50;
-export const DEFAULT_TIMEZONE = "Pacific/Palau";
